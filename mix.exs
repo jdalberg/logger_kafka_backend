acd427305c2924f700e51aa3f9afa84676185ae8
Jesper Dalberg <jdalberg@gmail.com>
2018-09-19 14:10:52 +0200

    Updated dependencies

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule LoggerKafkaBackend.Mixfile do
 
   def project do
     [app: :logger_kafka_backend,
-     version: "0.1.16",
-     elixir: ">= 1.7.2",
+     version: "0.1.17",
+     elixir: ">= 1.7.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description(),
@@ -19,7 +19,7 @@ defmodule LoggerKafkaBackend.Mixfile do
   defp deps do
     [
      {:brod, "~> 2.3.3"},
-     {:poison, "~> 2.0"},
+     {:poison, "~> 4.0.1"},
      {:meck, "~> 0.8.2", only: :test}, # To make test emulate calls to brod
      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
