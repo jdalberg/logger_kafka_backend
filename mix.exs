74212d635361f3f15c1a675cdc76fb69cfab5be6
Jesper Dalberg <jdalberg@gmail.com>
2019-03-19 13:29:59 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LoggerKafkaBackend.Mixfile do
 
   def project do
     [app: :logger_kafka_backend,
-     version: "0.1.21",
+     version: "0.1.22",
      elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
